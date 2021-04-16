189d48dd9c832ec3dbafbbc9bd8c688fd2e12e3a
skulsta <sti.sku@gmail.com>
2021-04-16 17:22:00 +0200

    Fetching the correct result from the API works

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TravelDeduction />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TravelDeduction from "./views/TravelDeduction/TravelDeduction.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TravelDeduction,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/TravelDeduction/TravelDeduction.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Reisefradrag</h1>
+  </div>
+</template>
+<script>
+import { onMounted } from "vue";
+import calculateDeduction from "../../functions/calculateDeduction";
+export default {
+  setup() {
+    onMounted(() => {
+      return calculateDeduction();
+    });
+  },
+};
+</script>
